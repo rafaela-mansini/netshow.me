4005b1205a0520646727414d06f68ae306ccb5dd
rafaela-mansini <rafaela_mansini@hotmail.com>
2020-09-19 09:31:31 -0300

    Enviando o arquivo por e-mail

--- app/Mail/EnviarEmailCadastro.php
+++ app/Mail/EnviarEmailCadastro.php
@@ -26,8 +26,13 @@ class EnviarEmailCadastro extends Mailable
     public function build()
     {
         return $this->from(env('MAIL_FROM_ADDRESS'), env('APP_NAME'))
+        ->replyTo($this->informacoes['email'], $this->informacoes['nome'])
         ->subject('Novo cadastro efetuado')
         ->markdown('email-cadastro')
+        ->attach($this->informacoes['arquivo'], [
+            'as' => $this->informacoes['arquivo']->getClientOriginalName(),
+            'mime' => $this->informacoes['arquivo']->getMimeType()
+        ])
         ->with(['informacoes' => $this->informacoes]);
     }
 }
